78e8c3bfdd28e795067428703deb4a76f0e7d2cf
Inh3ritance <herreralaces@gmail.com>
2020-12-04 12:04:44 -0800

    Updated to react jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,79 @@
-import "./canvas.js";
 import "./index.css";
-import React from 'react';
-import {clear} from "./clear.js";
+import React, { Component } from 'react';
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state= {
+      lastPosition:null,
+      drawing:false,
+    };
+    this.canvasRef = React.createRef();
+    this.clear = this.clear.bind(this);
+    this.predict = this.predict.bind(this);
+    this.moving = this.moving.bind(this);
+  }
 
-function App() {
-  return (
-    <div>
-    <script src="canvas.js"></script>
-    	<h1>My Math Solver</h1>
-      <canvas id="canvas" width="600" height="400"></canvas>
-    	<div>
-    		<button id="clear" onClick={clear}>Clear</button>
-    		<button id="save">Save</button>
-    	</div>
-    </div>
-  );
+  handleEvent = (event) => {
+    if (event.type === "mousedown") {
+           this.setState({ drawing: true});
+       } else {
+           this.setState({ drawing: false});
+       }
+   }
+
+  clear() {
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#FFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+  
+  predict(){
+
+
+    
+    /* let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', 'CanvasAsImage.png');
+    const canvas = this.canvasRef.current;
+    canvas.toBlob(function(blob) {
+      let url = URL.createObjectURL(blob);
+      downloadLink.setAttribute('href', url);
+      downloadLink.click();
+    }); */
+  }
+
+  moving(e) {
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+      var pos = {
+        x: e.clientX - canvas.getBoundingClientRect().left,
+        y: e.clientY - canvas.getBoundingClientRect().top
+      };
+    if (this.state.lastPosition !== null && this.state.drawing) {
+      ctx.lineWidth = 10;
+      ctx.lineCap = "round";
+      ctx.beginPath();
+      ctx.fillStyle = "#000000";
+      ctx.moveTo(this.state.lastPosition.x, this.state.lastPosition.y);
+      ctx.lineTo(pos.x, pos.y);
+      ctx.closePath();
+      ctx.stroke();
+    }
+    this.setState({lastPosition: pos});
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>My Math Solver</h1>
+        <canvas ref={this.canvasRef} width="600" height="400" onMouseMove={e => this.moving(e)} onMouseUp={this.handleEvent} onMouseDown={this.handleEvent}> </canvas>
+        <button onClick={this.clear}>Clear</button>
+        <button onClick={this.save}>Save</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/clear.js
+++
@@ -1,7 +0,0 @@
-export const clear = () => {
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#FFFF";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
