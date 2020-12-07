6c3d02a50da291950078d8b0cb1fe0274ff70dbf
Jennifer Chiang <77jennifer77@users.noreply.github.com>
2020-12-06 21:22:30 -0800

    resize image to proper scale

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import * as tf from '@tensorflow/tfjs';
 
 class App extends Component {
-  
+
   constructor(props){
     super(props);
     this.state= {
@@ -14,7 +14,11 @@ class App extends Component {
     this.clear = this.clear.bind(this);
     this.predict = this.predict.bind(this);
     this.moving = this.moving.bind(this);
-    this.split = this.split.bind(this);
+  }
+
+  async componentDidMount(){
+    console.log(tf);
+    const model = await tf.loadLayersModel('https://storage.googleapis.com/mathsolvermodel/model.json');
   }
 
   handleEvent = (event) => {
@@ -25,22 +29,6 @@ class App extends Component {
        }
    }
 
-  split(n){
-    const canvas = this.canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const width = canvas.width/n;
-    const splits = [];
-
-    const img = new Image();
-    for(var i = 0; i < n-1; i++){
-      //ctx.drawimage(this/*img*/, i*width, 0, (i+1)*width, canvas.height );
-      img.src = canvas.toDataURL();
-      splits.push(img);
-    }
-    console.log(splits);
-    return splits; //returns an array of Elemets <img>
-  }
-
   clear() {
     const canvas = this.canvasRef.current;
     const ctx = canvas.getContext("2d");
@@ -48,29 +36,27 @@ class App extends Component {
     ctx.fillStyle = "#FFFF";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
-  
+
   async predict(){
     const model = await tf.loadLayersModel('https://storage.googleapis.com/mathsolvermodel/model.json');
     const canvas = this.canvasRef.current;
-    this.split(2);
 
-    canvas.width = 32
-    canvas.height = 32
- 
-     const resized_grayscale_Tensor = tf.browser.fromPixels(canvas)
+    // resizing code
+    var resizedCanvas = document.createElement("canvas");
+    var resizedContext = resizedCanvas.getContext("2d");
+
+    resizedCanvas.height = "32";
+    resizedCanvas.width = "32";
+
+    resizedContext.drawImage(canvas, 0, 0, 32, 32);
+    var myResizedData = resizedCanvas.toDataURL();
+    // document.body.appendChild(resizedCanvas);
+
+     const resized_grayscale_Tensor = tf.browser.fromPixels(resizedCanvas)
     .mean(2)
     .toFloat()
     .expandDims(0)
     .expandDims(-1);
-
-    
-    let downloadLink = document.createElement('a');
-    downloadLink.setAttribute('download', 'CanvasAsImage.png');
-    canvas.toBlob(function(blob) {
-      let url = URL.createObjectURL(blob);
-      downloadLink.setAttribute('href', url);
-      downloadLink.click();
-    });
 
     console.log(resized_grayscale_Tensor);
 
@@ -78,7 +64,7 @@ class App extends Component {
 
     var value = prediction.dataSync()
 
-    console.log(value) 
+    console.log(value)
 
   }
 
